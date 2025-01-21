2fa8fd50291ffdc24fac5ee3d8837beda9e570c8
Cyrus Yiu <UnsignedArduino@outlook.com>
2025-01-21 15:11:59 -0500

    Test scrolling

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,34 @@
 #include <Arduino.h>
+#include <MD_MAX72xx.h>
+#include <MD_Parola.h>
+
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+#define MAX_DEVICES 16
+
+#define CLK_PIN 11
+#define DATA_PIN 13
+#define CS_PIN 12
+
+MD_Parola p = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+
 void setup() {
   Serial.begin(115200);
   pinMode(48, OUTPUT);
   digitalWrite(48, LOW);
 
+  p.begin();
+  p.setIntensity(1);
+  p.setSpeed(20);
+
   while (true) {
-    digitalWrite(48, HIGH);
-    delay(500);
-    digitalWrite(48, LOW);
-    delay(500);
+    if (p.displayAnimate()) {
+      p.displayText("Hello, World! Here is a really long message to test "
+                    "scrolling text.",
+                    PA_LEFT, p.getSpeed(), 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
+    }
   }
 }
 
 void loop() {
-  // write your code here
+  // Using while loop in setup to mimic loop because actually using loop crashes
 }
